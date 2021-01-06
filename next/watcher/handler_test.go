6ed7aff92f860c45672fc882fb666812aa2fef64
Maksym Nazarenko <maks.nazarenko@gmail.com>
2021-01-06 14:47:10 +0200

    add handler tests

---
+++ next/watcher/handler_test.go
@@ -0,0 +1,24 @@
+package watcher
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/maxim-nazarenko/nextshop-item-watcher/next"
+)
+
+func TestHandleFuncWrapper_wrapsPlainFunction(t *testing.T) {
+	var actualString string
+
+	option := next.ItemOption{Name: "test item", Number: 29, Price: "12 euros", StockStatusString: "InStock"}
+	expectedString := option.String()
+
+	wrapper := HandleFuncWrapper{handleFunc: func(options ...next.ItemOption) {
+		actualString = options[0].String()
+	}}
+
+	wrapper.Handle(option)
+
+	assert.Equal(t, expectedString, actualString)
+}
