fd4bc1581a27913c93436650e98c62248b1645f4
Maksym Nazarenko <maks.nazarenko@gmail.com>
2021-01-06 14:39:25 +0200

    add item tests

---
+++ next/item_test.go
@@ -0,0 +1,25 @@
+package next
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNormalizeArticle(t *testing.T) {
+
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "123-456", expected: "123456"},
+		{input: "0_5-23-4", expected: "05234"},
+		{input: "", expected: ""},
+	}
+
+	for _, test := range tests {
+		t.Run("Normalize <"+test.input+">", func(innerT *testing.T) {
+			assert.Equal(innerT, test.expected, NormalizeArticle(test.input))
+		})
+	}
+}
