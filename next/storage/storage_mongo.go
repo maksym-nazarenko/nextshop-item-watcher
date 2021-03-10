db981bd2a5b89d516de85ea87f44c116bd56379d
Maksym Nazarenko <maks.nazarenko@gmail.com>
2021-03-10 21:23:16 +0200

    remove unused code

--- next/storage/storage_mongo.go
+++ next/storage/storage_mongo.go
@@ -205,16 +205,6 @@ func (m *MongoStorage) RemoveSubscription(item subscription.Item) (bool, error) 
 	return true, nil
 }
 
-func (m *MongoStorage) disconnect() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	if err := m.client.Disconnect(ctx); err != nil {
-		return err
-	}
-
-	return nil
-}
-
 func NewMongo(uri string) (*MongoStorage, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
